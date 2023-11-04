163fd2753467508b0eeaeabe04dd96eaf63db7f1
Elena <elicfr@mail.ru>
2023-11-04 23:30:46 +0300

    create simple models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag, ProductGroup, Product, Shop, Purchase
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "description", "created_by"
+    list_display_links = "pk", "name"
+
+
+@admin.register(ProductGroup)
+class ProductGroupAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "description", "created_by"
+    list_display_links = "pk", "name"
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "group"
+    list_display_links = "pk", "name"
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "description", "created_by"
+    list_display_links = "pk", "name"
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    def date_output(self, obj):
+        return obj.date.strftime("%Y-%m-%d")
+
+    date_output.admin_order_field = 'date'
+    date_output.short_description = 'Date output'
+    list_display = "pk", "date_output", "product","quantity", "total", "shop", "user"
+    list_display_links = "pk", "product"
+
+
+
+
--- main/models.py
+++ main/models.py
@@ -1,10 +1,23 @@
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, default="default")
+    description = models.TextField(null=True, blank=True)
+    created_by = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class ProductGroup(models.Model):
     name = models.CharField(max_length=50, default="default")
     description = models.TextField(null=False, blank=True, default="default food group description")
+    created_by = models.ForeignKey(User, models.CASCADE)
+    # tags
 
     def __str__(self):
         return self.name
@@ -12,11 +25,49 @@ class ProductGroup(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    age = models.PositiveIntegerField(null=True)
     group = models.ForeignKey(
         ProductGroup,
         on_delete=models.PROTECT,
     )
 
+    class Unit(models.TextChoices):
+        PIECE = "PC", _("Штука")
+        KILOGRAM = "KG", _("Килограмм")
+        LITER = "L", _("Литр")
+
+    unit = models.CharField(
+        max_length=2,
+        choices=Unit.choices,
+        default=Unit.PIECE,
+    )
+
     def __str__(self):
         return self.name
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=50, default="default")
+    description = models.TextField(null=False, blank=True, default="default shop description")
+    created_by = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Purchase(models.Model):
+    date = models.DateField()
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT,
+    )
+    quantity = models.FloatField(default=1)
+    #
+    total = models.FloatField(default=0)
+    shop = models.ForeignKey(
+        Shop,
+        on_delete=models.PROTECT,
+    )
+    user = models.ForeignKey(User, models.CASCADE)
+
+    def __str__(self):
+        return f"{self.date}, {self.shop}, {self.product}, {self.user.username}"
